533870680ac1147f439ce743dd5d5ff338571504
MkSerdyuk <git_user@mail.ru>
2022-05-22 11:50:30 +0300

    Доделан скрипт для сбора домов

--- data-collection-houses.py
+++ data-collection-houses.py
@@ -6,17 +6,16 @@ from fake_useragent import UserAgent
 # Получение ссылки
 location = input('Введите субъект федерации (транслитом) ') # субект федерации
 city = input('Введите город (транслитом) ') # город
-url = 'https://dom.mingkh.ru/krym/'+location+'/'+city
+url = 'https://dom.mingkh.ru/'+location+'/'+city+'/houses'
 print(url)
 
-f = open('table.csv', 'w')
+f = open('houses.csv', 'w')
 
 def scrapData(url):
     global f
     request = requests.get(url, headers={'User-Agent': UserAgent().chrome})
     soup = BeautifulSoup(request.text)
     table = soup.find('table')
-
     output_rows = []
     for table_row in table.findAll('tr'):
         columns = table_row.findAll('td')
@@ -24,30 +23,32 @@ def scrapData(url):
         for column in columns:
             output_row.append(column.text)
         output_rows.append(output_row)
+    if len(output_rows) == 1: #если длина 1, то колонок нет
+        return False
     del(output_rows[0])
     for p in output_rows:
-      for t in p:
-        t=t.replace(';', ',')
-        f.write(t[:-1]+';')
-      f.write('\n')
+        if not '—' in p: #если в строке пропуск, то она повреждена
+            for t in p:
+                t=t.replace(';', ',')
+                f.write(t+';')
+            f.write('\n')
+    return True
 
 #сбор данных со всех страниц
 
-scrapData(url) #собираем данные с первой страницы
-
 try:
     scrapData(url) #собираем данные с первой страницы
+    print('Страница',1,'загружена')
+    i = 2 #номер страницы
+
+    pUrl = url + '?page=' + str(i) #получаем ссылку на страницу
+
+    while scrapData(pUrl):
+        print('Страница',i,'загружена')
+        i+=1
+        pUrl = url + '?page=' + str(i) #получаем ссылку на страницу
 except:
     print('[Error] Такого города нет')
-
-i = 2 #номер страницы
-while True:
-    try:
-        pUrl = url + '?page=' + str(i) #получаем ссылку на страницу
-        scrapData(pUrl)
-        i+=1
-    except: #как только появляется ошибка, значит страницы с нужным номером уже нет
-        break
 
 f.close()
 print('Данные успешно загружены')
