1fbaf2a395823ce46f515fc55a3a6f6005342b90
MkSerdyuk <git_user@mail.ru>
2022-05-28 12:12:31 +0300

    Теперь вместо адреса записваются географические координаты

--- Data-Collection/data-collection-houses.py
+++ Data-Collection/data-collection-houses.py
@@ -1,35 +1,51 @@
 import pandas as pd
 import requests
+from geopy.geocoders import Nominatim
 from bs4 import BeautifulSoup
 
 # Получение ссылки
 location = input('Введите субъект федерации (транслитом) ') # субект федерации
 city = input('Введите город (транслитом) ') # город
 url = 'https://dom.mingkh.ru/'+location+'/'+city+'/houses'
+loc = Nominatim(user_agent="GetLoc")
 print(url)
 
 f = open('Houses.csv', 'w')
+
+def convertAdrToCoord(street):
+    global loc
+    try:
+        getLoc = loc.geocode(street)
+        lat = getLoc.latitude
+        lon = getLoc.longitude
+        return [lat, lon]
+    except:
+        return -1
+
 
 def scrapData(url):
     global f
     request = requests.get(url)
     soup = BeautifulSoup(request.text)
     table = soup.find('table')
-    output_rows = []
+    if len(table.findAll('tr')) == 1: #если длина 1, то остался только заголовок
+        return False
     for table_row in table.findAll('tr'):
         columns = table_row.findAll('td')
         output_row = []
-        for column in columns:
-            output_row.append(column.text)
-        output_rows.append(output_row)
-    if len(output_rows) == 1: #если длина 1, то колонок нет
-        return False
-    del(output_rows[0])
-    for p in output_rows:
-        if not '—' in p: #если в строке пропуск, то она повреждена
-            for t in p:
-                t=t.replace(';', ',')
-                f.write(t+';')
+        for i in range(1, len(columns)):
+            if i == 2: #3-я колонка на сайте - адресс
+                coord = convertAdrToCoord(columns[i].text)
+                if (coord != -1): #если есть координаты
+                    output_row += coord
+                else:
+                    output_row = []
+                    break
+            else:
+                output_row.append(columns[i].text)
+        if not '—' in output_row and output_row != []: #если в строке пропуск, то она повреждена
+            for t in output_row:
+                f.write(str(t)+';')
             f.write('\n')
     return True
 
@@ -38,16 +54,17 @@ def scrapData(url):
 try:
     scrapData(url) #собираем данные с первой страницы
     print('Страница',1,'загружена')
-    i = 2 #номер страницы
 
+    i = 2 #номер страницы
     pUrl = url + '?page=' + str(i) #получаем ссылку на страницу
 
     while scrapData(pUrl):
         print('Страница',i,'загружена')
         i+=1
         pUrl = url + '?page=' + str(i) #получаем ссылку на страницу
-except:
-    print('[Error] Такого города нет')
+except Exception as e:
+    print(e)
 
 f.close()
 print('Данные успешно загружены')
+input()
